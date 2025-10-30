3253123429d95bb5354483ca230a05649eb8144e
Ant <Kozakanton@i.ua>
2025-10-30 09:40:15 +0100

    Learning Vue  routning

---
+++ Vue_Router/src/App.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div id="app">
+    <h1>Hello bitch</h1>
+  </div>
+</template>
+
+<style>
+#app {
+  background-color: #2d2d2d;
+  min-height: 100vh;
+  color: white;
+  font-family: Arial, sans-serif;
+}
+
+h1 {
+  text-align: center;
+  margin: 0;
+}
+</style>
